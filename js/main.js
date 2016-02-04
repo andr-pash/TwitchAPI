9a9e381fa988b83bc0c547fdc91b4bb2a14c6b54
andr-pash <andreas.pasha@googlemail.com>
2016-02-04 23:35:13 +0100

    search functionality added
    …but loading of provided list on startup removed. needs to be redone.
    also exact search for specific channel needs to be added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $("document").ready(function() {
 
+  //******************* VARIABLES ********************************************//
+
   // variable setUp
   var streamer, channel, channelName, streamDesrc, thumbnail, channelLink, channelIdName;
   var z = 0;
@@ -11,21 +13,45 @@ $("document").ready(function() {
   var baseUrl = 'https://api.twitch.tv/kraken';
 
 
-  // function to populate DOM
-  function populateChannel(data) {
-    channelName = data.display_name;
-    channelIdName = data.name;
-    if (data.status) {
-      streamDesrc = data.status;
-    } else {
-      streamDesrc = "Description unavailable.";
-    }
-    channelLink = data.url;
-    thumbnail = data.logo;
+  //******************* MAIN FUNCTIONS TO HANDLE AJAX AND POPULATE DOM *******//
 
-    // template has to be inside func and after variables to work
-    itemTemplate =
-      `         <div class="item">
+  //load list of channels provided on start up
+  
+
+
+  // search for streams
+  function startSearch() {
+    var search = $(".searchbar").val();
+    console.log(search);
+    if (search.length > 2) {
+      $.ajax({
+        url: baseUrl + "/search/channels?limit=25&q=" + search,
+        dataType: "jsonp",
+        success: populateChannel,
+        error: console.log("fuck"),
+      });
+    }
+  }
+
+  // function to populate DOM
+  function populateChannel(obj) {
+    $(".item").remove();
+    var arr = obj.channels;
+    for (var i = 0; i < arr.length; i++) {
+      var data = arr[i];
+      channelName = data.display_name;
+      channelIdName = data.name;
+      if (data.status) {
+        streamDesrc = data.status;
+      } else {
+        streamDesrc = "Description unavailable.";
+      }
+      channelLink = data.url;
+      thumbnail = data.logo;
+
+      // template has to be inside func and after variables to work
+      itemTemplate =
+        `         <div class="item">
                 <div class="indicator offline" id="${channelIdName}"></div>
                 <div class="text">
                   <h4 class="text--title">${channelName}</h4>
@@ -34,17 +60,18 @@ $("document").ready(function() {
                 <a href="${channelLink}" class="logo" style="background-image: '${thumbnail}'">
                 </a>
               </div>`;
-    $(".list").append(itemTemplate);
+      $(".list").append(itemTemplate);
 
 
-    // call to api to check if channel is online
-    streamurl = baseUrl + "/streams/" + channelIdName;
-    $.ajax({
-      url: streamurl,
-      dataType: "jsonp",
-      success: checkOnline,
-      error: errorStream
-    });
+      // call to api to check if channel is online
+      streamurl = baseUrl + "/streams/" + channelIdName;
+      $.ajax({
+        url: streamurl,
+        dataType: "jsonp",
+        success: checkOnline,
+        error: errorStream
+      });
+    }
   }
 
 
@@ -52,8 +79,8 @@ $("document").ready(function() {
   function checkOnline(data) {
     if (data.stream) {
       var tempName = data.stream.channel.name;
-      $("#"+tempName).removeClass("offline");
-      $("#"+tempName).addClass("online");
+      $("#" + tempName).removeClass("offline");
+      $("#" + tempName).addClass("online");
     }
     console.log(data);
   }
@@ -63,62 +90,35 @@ $("document").ready(function() {
   function errorChannel() {
     console.log("error loading channels");
   }
+
   function errorStream() {
     console.log("error loading streams");
   }
 
 
-  // main call to api for information on channels specified in channels array
-  for (var i = 0; i < channels.length; i++) {
-
-    channel = channels[i];
-
-    channelurl = baseUrl + "/channels/" + channel;
-    $.ajax({
-      "url": channelurl,
-      dataType: "jsonp",
-      success: populateChannel,
-      error: errorChannel,
-    });
-  }
-
-
-  // search for streams
-  $(".searchbar").keypress(function(){
-
-    var search = $(".searchbar").val();
-    console.log(search);
-    if(search.length > 2){
-    $.ajax({
-      url: baseUrl + "/search/channels?limit=25&q=" + search,
-      dataType: "jsonp",
-      success: searchReturn,
-      error: console.log("fuck"),
-    });
-  }
-  });
-
-  function searchReturn(data){
-    console.log("this is the data");
-    console.log(data);
-
-  }
 
 
   //******************* User Interaction *****************************//
 
   // show/hide online offline channels
 
-  $("button.online").on("click", function(){
+  $("button.online").on("click", function() {
     $(".item").show();
     $(".offline").closest(".item").hide();
   });
-  $("button.offline").on("click", function(){
+  $("button.offline").on("click", function() {
     $(".item").show();
     $(".online").closest(".item").hide();
   });
-  $("button.all").on("click", function(){
+  $("button.all").on("click", function() {
     $(".item").show();
   });
 
+  // start search
+  $("button.go").click(startSearch);
+  $(".searchbar").keypress(function(e){
+    if(e.which == 13){
+      startSearch();
+    }
+  });
 });
